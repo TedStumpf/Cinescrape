3f837e2f5f5666de72a76d7628f17ef3dd20101d
Theodore Stumpf <theodorestumpf@gmail.com>
2018-11-24 19:10:02 -0500

    Added skeleton functions

--- imdb_helper.py
+++ imdb_helper.py
@@ -1,3 +1,26 @@
-#	imdb_helper.py
-#	A helper class for the IMDB API
-#	https://imdbpy.sourceforge.io/     (pip install imdbpy)
+#   imdb_helper.py
+#   A helper class for the IMDB API
+#   https://imdbpy.sourceforge.io/     (pip install imdbpy)
+
+class IMDb_Movie():
+
+    def __init__(self, movie_name):
+        pass
+
+    def get_directors(self):
+        pass
+
+    def get_full_title(self):
+        pass
+
+    def get_actors(self):
+        pass
+
+    def get_release_info(self):
+        pass
+
+    def get_ratings(self):
+        pass
+
+def get_movie_list(movie_name, count = 5):
+    pass
