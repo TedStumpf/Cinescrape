f1ac8f49492401e5f0c264854b53315163f04436
Theodore Stumpf <theodorestumpf@gmail.com>
2018-11-26 11:06:07 -0500

    Added __init__ function

--- imdb_helper.py
+++ imdb_helper.py
@@ -1,13 +1,22 @@
 #   imdb_helper.py
 #   A helper class for the IMDB API
 #   https://imdbpy.sourceforge.io/     (pip install imdbpy)
+import imdb
 
 class IMDb_Movie():
 
     def __init__(self, movie_name):
-        pass
+        dbobj = imdb.IMDb()
+        extra_info = ['awards', 'critic reviews', 'external reviews', 'full credits', 'keywords', 'main', 'quotes', 'release dates', 'release info']
+        
+        search_result = dbobj.search_movie(movie_name)
+        if (len(search_result) > 0):
+            self.movie = search_result[0]
+            dbobj.update(self.movie, info = extra_info)
+        else:
+            self.movie = None
 
-    def get_directors(self):
+    def get_director_names(self):
         pass
 
     def get_full_title(self):
